0b30413d9c5de5a9530df79f45244d6a12cec542
jim_doyle <jim_doyle@56530abd-8947-4513-bbf7-f0464c63a0cc>
2012-06-03 19:46:27 +0000

    new Unit test demonstrates full use case exploitation of API
    git-svn-id: http://svn.code.sf.net/p/nextbusapi/code/trunk@22 56530abd-8947-4513-bbf7-f0464c63a0cc

--- adapter/src/test/java/net/sf/nextbus/publicxmlfeed/impl/DemoTest.java
+++ adapter/src/test/java/net/sf/nextbus/publicxmlfeed/impl/DemoTest.java
@@ -11,7 +11,9 @@ import java.util.*;
 
 import org.junit.Before;
 /**
- *
+ * This simple unit test demonstrates full use of the API to retrieve bus
+ * activity close to the users' location.
+ * 
  * @author jrd
  */
 public class DemoTest {
@@ -30,35 +32,43 @@ public class DemoTest {
          Agency mbta = svc.getAgency("mbta");
          List<Route> mbtaRoutes  = svc.getRoutes(mbta);
          
-         // Choose the MBTA 110 - as I am near it right now....
-         Route the110 = Route.find(mbtaRoutes, "110");
-         RouteConfiguration rt110 = svc.getRouteConfiguration(the110);
-         List<Stop> stopsOnThe110 = rt110.getStops();
+         // Choose the MBTA 100 Bus - as I am near it right now....
+         Route route = Route.find(mbtaRoutes, "110");
+         RouteConfiguration routeConf = svc.getRouteConfiguration(route);
+         List<Stop> stops = routeConf.getStops();
          
          // Find the 3 nearest stops from me in under 1/2 mi
          // On a phone, I'd get my current GPS Locn from a Machine register.
-         Geolocation here = new Geolocation(42.420391, -71.093691);
-         List<Stop> closest2Me = Geolocation.sortedByClosest(stopsOnThe110, here, 3, 45);
+         Geolocation here = new Geolocation(42.42121, -71.09336);
+         List<Stop> closest2Me = Geolocation.sortedByClosest(stops, here, 10, 45);
        
          // For those three closest stops, get the current Arrival Predictions
          List<PredictionGroup> pdxns = svc.getPredictions(closest2Me);
          
          
          // Report on what we've found
-         System.out.println("For Route 110 there are "+closest2Me.size()+" nearby stops");
-         System.out.println(" They are are ");
+         System.out.println("For Route 101 there are "+closest2Me.size()+" nearby stops");
+         System.out.println(" The three closest stop to you are: ");
          for (Stop s: closest2Me) {
              System.out.println("   "+s.getTitle()+"  "+s.getGeolocation().getDistanceInMiles(here)+" miles away");
              
          }
+         System.out.println("Arrival predictions follow for your closest stops");
+         System.out.println("*************************************************");
          for (PredictionGroup g: pdxns) {
-             for (PredictionGroup.PredictionDirection d: g.getDirections()) {
-                for (Prediction p : d.getPredictions()) {
-                    System.out.println("   "+d.getTitle()+" "+p.getPredictedArrivalOrDepartureMinutes());
+             for (PredictionGroup.PredictionDirection pd: g.getDirections()) {
+                for (Prediction p : pd.getPredictions()) {
+                    Direction d = routeConf.getDirectionById(p.getDirectionId());
+                    System.out.printf("%s %s   %s %tk:%<tM \n",d.getTitle(), d.getName(), p.getPredictedArrivalOrDepartureMinutes(), p.getPredictedArrivalOrDepartureTimeUTC());
                 }
              }
          }
-         
+         System.out.println("Vehicles on route");
+         System.out.println("*****************");
+         for (VehicleLocation v: svc.getVehicleLocations(route, 0)) {
+             Direction d = routeConf.getDirectionById(v.getDirectionId());
+             System.out.printf("%s %s %.1f miles away from ,e.\n",v.getVehicle(), d, v.getGeolocation().getDistanceInMiles(here));
+         }
          
      }
 }
