d80452589f58adda8117dc4b931688bbeaab6015
jim_doyle <jim_doyle@56530abd-8947-4513-bbf7-f0464c63a0cc>
2012-09-10 00:17:10 +0000

    Initial import of JMS->JDBC VehicleLocations Logger
    git-svn-id: http://svn.code.sf.net/p/nextbusapi/code/trunk@69 56530abd-8947-4513-bbf7-f0464c63a0cc

---
+++ nextbus-logger/src/main/examples/create-vehlcn-table.sql
@@ -0,0 +1,15 @@
+-- SQL DDL for NextBus Vehicle Location Recorder (Apache Derby  or DB2)
+-- Note is has no Primary Key, or Indices, as it is intended for rapid insert
+DROP TABLE NEXTBUS.VEHLCN;
+CREATE TABLE NEXTBUS.VEHLCN (
+        AGENCY  CHAR(8) NOT NULL,
+        ROUTE   CHAR(12) NOT NULL,
+        VEHICLE CHAR(12) NOT NULL,
+        LONGITUDE DOUBLE,
+        LATITUDE DOUBLE,
+        HEADING DOUBLE,
+        LASTREPORT BIGINT,
+        TS BIGINT,
+        TSKEW BIGINT,
+        CREATED TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
---
+++ nextbus-logger/src/main/java/net/sf/nextbus/nextbuslogger/Main.java
@@ -0,0 +1,13 @@
+package net.sf.nextbus.nextbuslogger;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+public class Main 
+{
+    private static ClassPathXmlApplicationContext springCtx;
+    public static void main( String[] args )
+    {
+        System.out.println("*** NextBus Vehicle Locations Logger Demo ***");
+        springCtx = new ClassPathXmlApplicationContext(new String[]{              
+                    "nextbus-logger-spring-context.xml"
+                });
+    }
+}
