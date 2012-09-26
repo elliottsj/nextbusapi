9c6778d5e4c88fe2a47197eec189fef41a20244a
jim_doyle <jim_doyle@56530abd-8947-4513-bbf7-f0464c63a0cc>
2012-09-26 14:53:42 +0000

    1.0.1 release cleanup
    git-svn-id: http://svn.code.sf.net/p/nextbusapi/code/trunk@94 56530abd-8947-4513-bbf7-f0464c63a0cc

--- nextbus-logger/src/main/java/net/sf/nextbus/nextbuslogger/Main.java
+++ nextbus-logger/src/main/java/net/sf/nextbus/nextbuslogger/Main.java
@@ -1,12 +1,15 @@
 package net.sf.nextbus.nextbuslogger;
+
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-public class Main 
-{
+
+public class Main {
+
     private static ClassPathXmlApplicationContext springCtx;
-    public static void main( String[] args )
-    {
+
+    public static void main(String[] args) {
         System.out.println("*** NextBus VehicleLocation Logger Demo ***");
-        springCtx = new ClassPathXmlApplicationContext(new String[]{              
+        springCtx = new ClassPathXmlApplicationContext(new String[]{
+                    "derbySqlExceptionCodes.xml",
                     "nextbus-logger-spring-context.xml"
                 });
     }
